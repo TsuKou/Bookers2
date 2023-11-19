47aca7d79e52c3cd4ca47ddc2bcae1dc5095ada9
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-11-19 14:13:51 +0000

    [FInishi]2023/11/19課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @user = User.new
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
